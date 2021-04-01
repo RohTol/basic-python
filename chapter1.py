6ebec97385028731f144b60d699d5bd6c98680a2
RohTol <rohan@tolani.me>
2021-04-01 16:40:26 -0500

    tolani

---
+++ chapter1.py
@@ -0,0 +1,10 @@
+name = input('Would you like to continue? ')
+if name == 'n' or name == 'no':
+    print('Exiting')
+
+if name == 'y' or name == 'yes':
+    print('Continuing ...')
+    print('Complete!')
+
+else:
+    print("Please try again with a yes or no")
